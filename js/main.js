c301d671e2ba5f3e982c678f43b2a494e38f26a3
hatem sakly <hatemsakli@gmail.com>
2022-09-17 11:35:21 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/* global $ alert, console */
+
+$(function() {
+
+    'use strict';
+
+    /* nicescroll */
+
+    $('html').niceScroll({
+
+        cursorcolor: 'var(--main-color)',
+        cursorwidth: 10,
+        cursorborderradius: 0,
+        cursorborder: '1px solid var(--main-color)'
+
+    });
+
+});
